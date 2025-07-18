d584a498fb9802386ac516737d3deaa82ec31641
NuhDemir <drmaa217@gmail.com>
2025-07-18 18:46:56 +0300

    new update package lock json

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,15 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-
-// .env dosyasının yolunu BELİRTMEYİ en başa alın
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-// ----- KONTROL İÇİN GEÇİCİ LOGLAMA -----
-// Bu satırı ekleyerek değişkenin yüklenip yüklenmediğini anında görün
 console.log("MONGO_URI Değeri:", process.env.MONGO_URI);
-// -----------------------------------------
 
 import { server } from "./src/lib/socket.js";
 import { connectDB } from "./src/lib/db.js";
 import logger from "./src/log/logger.js";
-// `app` importu bu dosyada gerekiyorsa kalsın, normalde socket.js içinden gelir.
 
 const PORT = process.env.PORT || 5000; // PORT bulunamazsa diye bir yedek değer atayın
 
